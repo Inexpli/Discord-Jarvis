9fdca272166de5b9ba6118134f65dcd0e9573352
Jakub <jezjakub.work@gmail.com>
2026-01-26 23:57:17 +0100

    update config.py to include Tavily API key and enhance system prompts for Polish language support

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN") # Bot token
 GUILD_ID = int(os.getenv("GUILD_ID")) # Server ID as integer
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY") # Tavily API Key
 HF_TOKEN = os.getenv("HF_TOKEN", "") # Optional
 
 
@@ -38,7 +39,7 @@ COMMON_IGNORED_PHRASES = [
     "mm", "mmh", "mmm", "hm", "hmm", "hmmm", "hmmmm", "hmmmmm" "uh", "uhm", 
     "uhmm", "uhmmm", "ehm", "ehmm", "ehmmm", "aha", "oho", 
     "ojej", "och", "oj", "hahaha", "hehehe", "hihihi", 
-    "hohoho", ".", "?", "!"
+    "hohoho", ".", "?", "!",
 ]
 
 if LANGUAGE.lower() == "en":
@@ -51,12 +52,14 @@ if LANGUAGE.lower() == "en":
     Your primary function is toassist users by answering questions, providing information, and engaging
     in meaningful conversations. You should be polite, concise, and informative in your responses. 
     Always strive to understand the user's intent and provide accurate and relevant information.
+    You have access to a tool "web_search" that allows you to search for information on the internet, which you can use 
+    when you do not know the answer to a user's question or when you need the most up-to-date information.
     The most important: Respond briefly and concisely.
     """.strip()
 
 elif LANGUAGE.lower() == "pl":
     IGNORED_PHRASES = [
-        "okej", "dobra", "tak", "nie"
+        "okej", "dobra", "tak", "nie", "Wszelkie prawa zastrzeżone", "Dziękuję."
     ]
     INITIAL_PROMPT = "Jarvis, słuchaj."
     SYSTEM_PROMPT = """
@@ -64,7 +67,9 @@ elif LANGUAGE.lower() == "pl":
     Twoją główną funkcją jest pomaganie użytkownikom poprzez odpowiadanie na pytania, dostarczanie informacji i 
     angażowanie się w znaczące rozmowy. Powinieneś być uprzejmy, zwięzły i informacyjny w swoich odpowiedziach. 
     Zawsze staraj się zrozumieć intencje użytkownika i dostarczać dokładne oraz istotne informacje.
-    Najważniejsze: Odpowiadaj krótko i zwięźle.
+    Masz dostęp do narzędzia "web_search" które pozwala ci wyszukiwać informacje w internecie, z którego możesz korzystać, 
+    gdy nie znasz odpowiedzi na pytanie użytkownika lub gdy potrzebujesz najnowszych informacji.
+    Najważniejsze: Odpowiadaj krótko i zwięźle. 
     """.strip()
 else:
     IGNORED_PHRASES = []
